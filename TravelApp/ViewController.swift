01613280031b6bda7e388beff387b4c0e2127e75
hady <A210124@students.gu.edu.lb>
2025-04-30 12:24:43 +0300

    basic commit

---
+++ TravelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravelApp
+//
+//  Created by hady ka on 30/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
