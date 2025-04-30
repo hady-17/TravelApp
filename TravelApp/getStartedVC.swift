bfe5e3b21e597d1814a13c9351dbea869b5e50c8
hady-17 <a210124@students.gu.edu.lb>
2025-04-30 13:00:47 +0300

    placing neccessary files to get started

---
+++ TravelApp/getStartedVC.swift
@@ -0,0 +1,29 @@
+//
+//  getStartedVC.swift
+//  TravelApp
+//
+//  Created by hady ka on 30/04/2025.
+//
+
+import UIKit
+
+class getStartedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
