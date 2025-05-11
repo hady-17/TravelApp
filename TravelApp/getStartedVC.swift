d95fc8f6cb35b55463e8f658e521cbf25b329b85
zenaromy <a210071@students.gu.edu.lb>
2025-05-11 21:21:39 +0300

    GetStarted

--- TravelApp/getStartedVC.swift
+++ TravelApp/getStartedVC.swift
@@ -1,11 +1,5 @@
-//
-//  getStartedVC.swift
-//  TravelApp
-//
-//  Created by hady ka on 30/04/2025.
-//
-
 import UIKit
+import Localize_Swift
 
 class getStartedVC: UIViewController {
 
@@ -13,9 +7,11 @@ class getStartedVC: UIViewController {
     @IBOutlet weak var backgroundImageView: UIImageView!
     @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var exploreLabel: UILabel!
     @IBOutlet weak var journeyButton: UIButton!
+    @IBOutlet weak var languageButton: UIButton!
 
-    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
@@ -24,66 +20,71 @@ class getStartedVC: UIViewController {
     
     private func setupUI() {
         
-        backgroundImageView.alpha = 0
-        logoImageView.alpha = 0
-        cardView.alpha = 0
-        cardView.transform = CGAffineTransform(translationX: 0, y: 100)
+        backgroundImageView.image = UIImage(named: "background")
+        backgroundImageView.contentMode = .scaleAspectFill
 
         
-        backgroundImageView.image = UIImage(named: "getStartedBackground")
-        logoImageView.image = UIImage(named: "travelinLogo")
+        logoImageView.image = UIImage(named: "travelin_logo")
+        logoImageView.contentMode = .scaleAspectFit
 
         
-        journeyButton.setTitle("Your Journey Starts Here", for: .normal)
+        cardView.layer.cornerRadius = 24
+        cardView.layer.shadowColor = UIColor.black.cgColor
+        cardView.layer.shadowOpacity = 0.1
+        cardView.layer.shadowOffset = CGSize(width: 0, height: 4)
+        cardView.layer.shadowRadius = 10
+
+        
+        exploreLabel.text = "Ready to explore beyond boundaries?".localized()
+        exploreLabel.textAlignment = .center
+        exploreLabel.font = UIFont.systemFont(ofSize: 20, weight: .semibold)
+        exploreLabel.textColor = .darkText
+
+        
+        journeyButton.setTitle("Your Journey Starts Here ✈️".localized(), for: .normal)
+        journeyButton.backgroundColor = UIColor.systemTeal
+        journeyButton.setTitleColor(.white, for: .normal)
         journeyButton.layer.cornerRadius = 12
-        journeyButton.clipsToBounds = true
         journeyButton.addTarget(self, action: #selector(journeyButtonTapped), for: .touchUpInside)
 
         
-        animateUI()
-        animateButtonPulse()
-    }
-
-    
-    private func animateUI() {
-        
-        UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseIn, animations: {
-            self.backgroundImageView.alpha = 1
-        })
+        languageButton.setTitle("English ⌄", for: .normal)
+        languageButton.setTitleColor(.white, for: .normal)
+        languageButton.addTarget(self, action: #selector(showLanguageOptions), for: .touchUpInside)
 
         
-        UIView.animate(withDuration: 1.0, delay: 0.3, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.5, options: [], animations: {
-            self.logoImageView.alpha = 1
-            self.logoImageView.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
-        }, completion: { _ in
-            UIView.animate(withDuration: 0.3) {
-                self.logoImageView.transform = .identity
-            }
-        })
-
-        
-        UIView.animate(withDuration: 1.0, delay: 0.6, options: .curveEaseOut, animations: {
-            self.cardView.alpha = 1
-            self.cardView.transform = .identity
-        })
+        animateEntrance()
     }
 
-    private func animateButtonPulse() {
-        let pulse = CASpringAnimation(keyPath: "transform.scale")
-        pulse.fromValue = 1.0
-        pulse.toValue = 1.05
-        pulse.duration = 0.6
-        pulse.autoreverses = true
-        pulse.repeatCount = .infinity
-        pulse.initialVelocity = 0.5
-        pulse.damping = 1.0
-
-        journeyButton.layer.add(pulse, forKey: "pulse")
-    }
-
-    
+   
     @objc private func journeyButtonTapped() {
         let signInVC = signInVC(nibName: "signInVC", bundle: nil)
         navigationController?.pushViewController(signInVC, animated: true)
+    }
+
+    @objc private func showLanguageOptions() {
+        let alert = UIAlertController(title: "Choose Language", message: nil, preferredStyle: .actionSheet)
+        let languages = ["en": "English", "fr": "Français", "ar": "العربية"]
+
+        for (code, name) in languages {
+            alert.addAction(UIAlertAction(title: name, style: .default, handler: { _ in
+                Localize.setCurrentLanguage(code)
+                self.setupUI() // refresh labels
+            }))
+        }
+
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(alert, animated: true)
+    }
+
+    
+    private func animateEntrance() {
+        cardView.alpha = 0
+        cardView.transform = CGAffineTransform(translationX: 0, y: 50)
+
+        UIView.animate(withDuration: 0.6, delay: 0.2, options: [.curveEaseOut], animations: {
+            self.cardView.alpha = 1
+            self.cardView.transform = .identity
+        }, completion: nil)
     }
 }
