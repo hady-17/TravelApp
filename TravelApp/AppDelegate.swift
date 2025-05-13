8c22b3043863b0d5bd68597b41a56f45e14a2f40
hady-17 <a210124@students.gu.edu.lb>
2025-05-13 21:54:54 +0300

    integrated with sara branch

--- TravelApp/AppDelegate.swift
+++ TravelApp/AppDelegate.swift
@@ -13,6 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         let accSB = UIStoryboard(name: "account", bundle: nil)
         let accVC = accSB.instantiateViewController(withIdentifier: "accountID")
         accVC.tabBarItem.image = UIImage(systemName: "person.circle")
